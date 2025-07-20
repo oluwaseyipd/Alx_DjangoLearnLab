af1aec8b2bee8b7d0bfb69aef819269ce1c90b30
Oluwaseyi <oluwaseyipd@gmail.com>
2025-07-20 12:09:08 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from relationships_app import views as relationship_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", user_views.register, name="register"),
     path("login/", user_views.login, name="login"),
     path("", include("bookshelf.urls")),
+     path('', include('relationship_app.urls')),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# 🔹 Function-Based View: List all books
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+# 🔹 Class-Based View: Show details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
