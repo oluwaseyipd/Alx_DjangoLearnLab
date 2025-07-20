a0c9c987846b57d65a1b2708f3d83f4ac0464143
Oluwaseyi <oluwaseyipd@gmail.com>
2025-07-20 12:28:25 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 # relationship_app/views.py
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # 🔹 Function-Based View: List all books
 def list_books(request):
