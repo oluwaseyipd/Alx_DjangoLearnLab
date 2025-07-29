45371d2c072a57b96871112776151bdb39023202
Oluwaseyi <oluwaseyipd@gmail.com>
2025-07-29 15:10:51 +0100

    3. Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,14 +3,15 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class BookList(generics.ListAPIView):
-    # queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        queryset = queryset = Book.objects.all()
+        queryset = Book.objects.all()
         name_filter = self.request.query_params.get('name', None)
         if name_filter is not None:
             queryset = queryset.filter(name__icontains=name_filter)
@@ -19,6 +20,7 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = super().get_queryset()
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework.authtoken",
     "api",
     "rest_framework",
     "django.contrib.admin",
@@ -122,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
