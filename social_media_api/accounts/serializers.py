bca6af5fa6fef9f20b89e70ad02443911f54888f
Oluwaseyi <oluwaseyipd@gmail.com>
2025-08-24 13:01:48 +0100

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,14 +10,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email", "bio", "profile_picture", "followers"]
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  
 
     class Meta:
         model = User
         fields = ["username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = get_user_model().objects.create_user(  
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
